a213ef1b237b37d8db40a10d8286e90b1e934013
Kimball <yang456@purdue.edu>
2014-10-18 13:29:04 -0400

    testing squcube.py functions

---
+++ squcube_test.py
@@ -0,0 +1,28 @@
+import random
+import unittest
+import squcube
+
+num = random.randint(1,100000)
+
+
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        pass
+ 
+    def test_numbers(self):
+        correct_squ= num*num
+        test_squ= squcube.square(num)
+        self.assertEqual( correct_squ,test_squ)
+        
+    def test_cube(self):
+        correct_squ= num*num*num
+        test_cube= squcube.cube(num)
+        self.assertEqual( correct_squ,test_cube)
+if __name__ == '__main__': 
+    unittest.main()# unittest.main() provides a command-line interface to the test scrip
+
+
+
+
+
