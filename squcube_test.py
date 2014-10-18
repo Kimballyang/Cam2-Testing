29fef3c855a531b4e631c4b6695157c163f1ce02
Kimball <yang456@purdue.edu>
2014-10-18 15:36:39 -0400

    update the code

--- squcube_test.py
+++ squcube_test.py
@@ -10,15 +10,15 @@ class Test(unittest.TestCase):
     def setUp(self):
         pass
  
-    def test_numbers(self):
+    '''def test_square(self):
         correct_squ= num*num
         test_squ= squcube.square(num)
-        self.assertEqual( correct_squ,test_squ)
+        self.assertEqual(correct_squ,test_squ)'''
         
     def test_cube(self):
-        correct_squ= num*num*num
+        correct_cube= num*num*num
         test_cube= squcube.cube(num)
-        self.assertEqual( correct_squ,test_cube)
+        self.assertEqual( correct_cube,test_cube)
 if __name__ == '__main__': 
     unittest.main()# unittest.main() provides a command-line interface to the test scrip
 
